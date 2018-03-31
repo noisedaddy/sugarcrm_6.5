152eb19291e067c111ac1a6e83680c4f17071454
root <root@localhost.localdomain>
2018-03-31 14:16:36 -0400

    fix, duplicate entries

--- custom/modules/Contacts/contacts_hook.php
+++ custom/modules/Contacts/contacts_hook.php
@@ -1,9 +1,17 @@
 <?php
 
-  //prevents directly accessing this file from a web browser
-    if(!defined('sugarEntry') ||!sugarEntry) die('Not A Valid Entry Point');
-    class add_name_class{
-        function add_name_method(& $focus, $event, $arguments){
+//prevents directly accessing this file from a web browser
+if (!defined('sugarEntry') || !sugarEntry)
+    die('Not A Valid Entry Point');
+
+class add_name_class {
+
+    function add_name_method(& $focus, $event, $arguments) {
+
+        if (!stristr($focus->first_name, "(this is appended)")) {
             $focus->first_name .= "(this is appended)";
         }
+        
     }
+
+}
