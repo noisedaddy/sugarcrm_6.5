a1d66226d792d22780c1983bc1e43d77bb3b09f0
root <veljko.radenkovic@gmail.com>
2018-04-01 06:29:32 -0400

    B. ScheduledTasks

--- custom/Extension/modules/Schedulers/Ext/ScheduledTasks/custom_job.php
+++ custom/Extension/modules/Schedulers/Ext/ScheduledTasks/custom_job.php
@@ -1,10 +1,12 @@
 <?php
 
    array_push($job_strings, 'custom_job');
-    function custom_job()
-    {
+   
+    function custom_job(){
+        
+        //For testing util function
         if (function_exists('word_count'))
-            $w = word_count(0);  
+            $w = word_count("one two three");  //or add an argument a number
         else 
             $w = 'func not exists';
         file_put_contents('sugarcrm.log', "I'm a ScheduledTask".$w, FILE_APPEND);
